d6c011de7073f83c9bccc1a775450bd2adf0a129
Anton Eitzinger - funkbag <55765620+funkbag@users.noreply.github.com>
2025-05-22 15:07:44 -0500

    Update app.py

--- lambda-rasterio-container/app.py
+++ lambda-rasterio-container/app.py
@@ -1,1 +1,8 @@
+import rasterio
+import numpy as np
 
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "Rasterio and Numpy loaded successfully!"
+    }
